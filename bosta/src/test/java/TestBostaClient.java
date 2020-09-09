34781a05d1103ca21c6012e8a9053ef07b8f7f42
Mohammad Sakr <mohammad@sakr>
2020-09-09 09:44:49 +0200

    Adding test to the library

---
+++ bosta/src/test/java/TestBostaClient.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+import com.bosta.BostaClient;
+
+public class TestBostaClient {
+
+	BostaClient client = 
+			new BostaClient("6fdd50c8c9234db89a60a807abb503bb49c461e6cb7b89df56b275b84718cf79");
+	@Test
+	public void testBostaClient() {
+		client.getDelivery("2774255");
+	}
+}
